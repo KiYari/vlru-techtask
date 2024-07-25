998ecceb487eda4a43e643b06815efd5b7efab7e
Kim Arthur <termrdz@gmail.com>
2024-07-25 10:50:52 +0300

    changed log line reading

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ import org.example.service.LogService;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
+    private static String logToProcess = "";
 
     public static void main(String[] args) {
         if (args.length < 4) {
@@ -33,18 +33,15 @@ public class Main {
         String line;
         try {
             while ((line = reader.readLine()) != null) {
-//                System.out.println("IM PROCESSING: ");
-//                System.out.println(line);
-//                System.out.println("ACCESSIBS: " + logService.accessibility + " " + logService.getCurrentLogsUnaccessible() + " " + logService.getCurrentLogsAccessible());
-                logService.processLog(line, timeout, accessibility, false);
+                if (!logToProcess.isEmpty()) {
+                    logService.processLog(logToProcess, timeout, accessibility, false);
+                }
+                logToProcess = line;
             }
+            logService.processLog(logToProcess, timeout, accessibility, true);
         } catch (Exception e) {
             throw new RuntimeException("Error occured while reading log lines");
         }
 
-    }
-
-    private Boolean isEndOfFile(BufferedReader reader) {
-        return true;
     }
 }
